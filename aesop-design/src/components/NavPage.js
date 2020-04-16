f99f274f499ce803b95b5ed5e8a14e208ac061d5
Lawrence Obote <dev.obote@gmail.com>
2020-04-16 16:10:27 -0400

    Created NavPage

---
+++ aesop-design/src/components/NavPage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class NavPage extends Component {
+    render() {
+        return (
+            <div>
+                <div></div>
+                <nav>
+                    <ul>
+                        <li>Shop</li>
+                        <li>Read</li>
+                        <li>Store</li>
+                        <li>Search</li>
+                    </ul>  
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default NavPage;
