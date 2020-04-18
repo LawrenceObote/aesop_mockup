712527ec998b57d9baff8fe2bee8019736ad33b4
Lawrence Obote <dev.obote@gmail.com>
2020-04-17 23:41:03 -0400

    A bunch of css

--- aesop-design/src/components/NavPage.js
+++ aesop-design/src/components/NavPage.js
@@ -3,18 +3,33 @@ import React, { Component } from 'react'
 export class NavPage extends Component {
     render() {
         return (
-            <div>
+            <div className="navPage">
                 <div>
+                
                     <h3 id="compBar">Complimentary standard shipping on all orders + </h3>
                 </div>
+                <div id="navList">
                 <nav>
                     <ul id="nav">
                         <li>Shop</li>
                         <li>Read</li>
                         <li>Store</li>
-                        <li>Search</li>
+                        <li>Search</li>  
+                    </ul>
+                    <ul>
+                        <li id="right">Login</li>
                     </ul>  
                 </nav>
+                </div>
+                <div id="aesop">
+                    <h2 className="spacing">Aesop</h2>
+                    <h2 className="spacing">Forumulations for skin, hair, home and body</h2>
+                </div>
+                <div id="sensory">
+                    <p className="spacingP">Created with sensory pleasures in mind</p>
+                </div>
+                
+
             </div>
         )
     }
